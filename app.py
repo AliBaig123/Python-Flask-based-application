4499618d86de0483a87bbc8ee68ee7aefa1245d6
Muhammad Ali Baig <136926207+AliBaig123@users.noreply.github.com>
2026-03-27 15:02:54 +0500

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Ali DEVOPS Engineer Intern (Dockerized with CICD pipeline with jenkins  Application new code pushed)"
+    return "Ali DEVOPS Engineer Intern (Dockerized with CICD pipeline with jenkins  Application new code Pushed)"
 
 if __name__ == "__main__":
    app.run(host="0.0.0.0", port=5000)
