81342db3ff60ada9a7e1ca8f4200b4522c7ed697
mabaig <muhammadalibaig885@gmail.com>
2026-03-31 12:24:23 +0500

    updated code

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "( Ali DEVOPS ENGINEER Intern at Blue Stone  Dockerized with CICD pipeline with jenkins  Application new updated Today code Pushed"
+    return "( Muhammad Ali DEVOPS ENGINEER Intern at Blue Stone  Dockerized with CICD pipeline with jenkins  Application new updated Today code Pushed"
 
 if __name__ == "__main__":
    app.run(host="0.0.0.0", port=5000)  
